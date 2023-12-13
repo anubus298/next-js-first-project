e6a269bd644cd17f2fd5fc46620d924e21c34d13
anubus298 <araristaf@gmail.com>
2023-12-13 18:11:00 +0100

    quick fix

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   env: {
     customUrl: "http://localhost:8000",
-    pocketBaseUrl: "https://safomart-app.pockethost.io/"
-      // process.env.NODE_ENV === "development"
-      //   ? "http://127.0.0.1:8090/"
-      //   : "https://safomart-app.pockethost.io/",
+    pocketBaseUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://127.0.0.1:8090/"
+        : "https://safomart-app.pockethost.io/",
   },
   experimental: {
     swcPlugins: [["@swc-jotai/react-refresh", {}]],
