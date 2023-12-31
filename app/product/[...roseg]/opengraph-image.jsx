82cb38f863596bcdd08846f1105d4c7b73ce709c
anubus298 <araristaf@gmail.com>
2023-12-31 16:27:48 +0100

    small fix

--- app/product/[...roseg]/opengraph-image.jsx
+++ app/product/[...roseg]/opengraph-image.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "next/server";
 
-import PocketBase from "pocketbase";
 export const alt = "safoMart";
 export const size = {
   width: 1200,
@@ -10,17 +9,7 @@ export const size = {
 
 export const contentType = "image/png";
 
-export default async function Image({ params }) {
-  const pb = new PocketBase(process.env.pocketBaseUrl);
-  const id = params.roseg[1];
-  const type =
-    "Pro" +
-    params.roseg[0][0].toUpperCase() +
-    params.roseg[0].slice(1, params.roseg[0].length);
-  const res = await pb.collection(type).getOne(id, {
-    fields: "name,imgs,id,collectionName",
-    cache: "force-cache",
-  });
+export default async function Image() {
   return new ImageResponse(
     (
       <div
@@ -34,13 +23,7 @@ export default async function Image({ params }) {
           justifyContent: "center",
         }}
       >
-        <img
-          src={`${process.env.pocketBaseUrl}api/files/${res.collectionId}/${res.id}/${res.imgs[0]}`}
-          height={500}
-          width={500}
-          alt={res.name}
-        />
-        <p>{res.name} is On Stocks !</p>
+        <p>On Stocks !</p>
       </div>
     ),
     {
